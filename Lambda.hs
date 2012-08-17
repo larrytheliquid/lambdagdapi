21b0e155e3bedc2a65bddaff803c8856a49dc88d
Larry Diehl <larrytheliquid@gmail.com>
2012-08-17 15:13:32 -0700

    iType

--- Lambda.hs
+++ Lambda.hs
@@ -1,5 +1,6 @@
 module Lambda where
 import Prelude hiding (print)
+import Control.Monad.Error
 
 data ITerm
    =  Ann    CTerm Type
@@ -33,7 +34,7 @@ data Name
   deriving (Show, Eq)
 
 vfree :: Name -> Value
-vfree n = VNeutral (NFree n)
+vfree x = VNeutral (NFree x)
 
 vapp :: Value -> Value -> Value
 vapp (VLam f) v = f v
@@ -43,7 +44,7 @@ type Env = [Value]
 
 iEval :: ITerm -> Env -> Value
 iEval (Ann ct _)  env = cEval ct env
-iEval (Free n)    env = vfree n
+iEval (Free x)    _   = vfree x
 iEval (Bound i)   env = env !! i
 iEval (it :@: ct) env = vapp (iEval it env) (cEval ct env)
 
@@ -51,6 +52,48 @@ cEval :: CTerm -> Env -> Value
 cEval (Inf it) env = iEval it env
 cEval (Lam ct) env = VLam (\v -> cEval ct (v:env))
 
+data Kind = Star
+  deriving (Show)
+
+data Info
+  = HasKind Kind
+  | HasType Type
+  deriving (Show)
+
+type Context = [(Name, Info)]
 type Result a = Either String a
 
+cKind :: Context -> Type -> Kind -> Result ()
+cKind ctx (TFree x) Star =
+  case lookup x ctx of
+    Just (HasKind Star) -> return ()
+    Just (HasType _)    -> undefined
+    Nothing             -> throwError ("unknown kind identifier: " ++ show x)
+cKind ctx (Fun tp tp') Star = do
+  cKind ctx tp  Star
+  cKind ctx tp' Star
 
+iType0 :: Context -> ITerm -> Result Type
+iType0 = iType 0
+
+iType :: Int -> Context -> ITerm -> Result Type
+iType i ctx (Ann ct tp) = do
+  cKind ctx tp Star
+  cType i ctx ct tp
+  return tp
+iType _ _ (Bound _) = undefined
+iType _ ctx (Free x) =
+  case lookup x ctx of
+    Just (HasKind Star) -> undefined
+    Just (HasType tp)   -> return tp
+    Nothing             -> throwError ("unknown type identifier: " ++ show x)
+iType i ctx (it :@: ct) = do
+  itp <- iType i ctx it
+  case itp of
+    Fun _ tp' -> do
+      cType i ctx ct tp'
+      return tp'
+    TFree x -> throwError ("illegal application against non-function: " ++ show x)
+
+cType :: Int -> Context -> CTerm -> Type -> Result ()
+cType = undefined
